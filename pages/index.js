ffcc1b35a31f68a6af8bcdeb316646afb376ea61
Wojciech Mosienski <wojciech.mosienski@itechart-group.com>
2021-08-11 15:33:28 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Logo from 'itechartwrapper/src/core/Logo'
+
+export default class extends Component {
+  render () {
+    return (
+      <Logo />
+    );
+  }
+}
