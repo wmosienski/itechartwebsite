ab3d23084098dfb67c8fbb4d5bfc22617bbd2dfd
Magdalena Oziębło <magdalena.ozieblo@itechart-group.com>
2021-08-13 15:09:57 +0200

    Updated version + demo example

---
+++ mocks/MENU_LINKS.js
@@ -0,0 +1,305 @@
+module.exports = [
+    {
+        title: 'Services',
+        route: 'services',
+        links: [
+            {
+                subtitle: 'Web',
+                route: 'services/web-development',
+                subLinks: [
+                    {
+                        subLink: 'Frontend Developer',
+                        route: 'services/frontend-development'
+                    },
+                    {
+                        subLink: 'Backend Developer',
+                        route: 'services/backend-development'
+                    }
+                ]
+            },
+            {
+                subtitle: 'Mobile',
+                route: 'services/mobile-development',
+                subLinks: [
+                    {
+                        subLink: 'iOS',
+                        route: 'services/ios-development'
+                    },
+                    {
+                        subLink: 'Android',
+                        route: 'services/android-development'
+                    },
+                    {
+                        subLink: 'Cross-platform',
+                        route: 'services/cross-platform-development'
+                    }
+                ]
+            },
+            {
+                subtitle: 'QA and testing',
+                route: 'services/software-testing',
+                subLinks: [
+                    {
+                        subLink: 'Test automation',
+                        route: 'services/test-automation'
+                    },
+                    {
+                        subLink: 'Mobile testing',
+                        route: 'services/mobile-testing'
+                    }
+                ]
+            },
+            {
+                subtitle: 'Cloud development',
+                route: 'services/cloud-development',
+                subLinks: [
+                    {
+                        subLink: 'Salesforce',
+                        route: 'salesforce'
+                    },
+                    {
+                        subLink: 'SaaS',
+                        route: 'services/saas-development'
+                    },
+                    {
+                        subLink: 'DevOps',
+                        route: 'services/devops-development'
+                    },
+                    {
+                        subLink: 'SharePoint/Office 365',
+                        route: 'services/sharepoint-development'
+                    }
+                ]
+            },
+            {
+                subtitle: 'UI/UX design',
+                route: 'services/ui-ux-design'
+            }
+        ],
+        isArrow: true
+    },
+    {
+        title: 'Skills',
+        route: 'skills',
+        links: [
+            {
+                subtitle: 'Open source',
+                route: 'skills/open-source',
+                subLinks: [
+                    {
+                        subLink: 'Python',
+                        route: 'skills/python-development-services'
+                    },
+                    {
+                        subLink: 'PHP',
+                        route: 'skills/php-development-services'
+                    },
+                    {
+                        subLink: 'Ruby on Rails',
+                        route: 'skills/ror-development-services'
+                    },
+                    {
+                        subLink: 'Golang',
+                        route: 'skills/golang-development-services'
+                    }
+                ]
+            },
+            {
+                subtitle: 'Time-tested',
+                route: 'skills/time-tested',
+                subLinks: [
+                    {
+                        subLink: '.NET',
+                        route: 'skills/dot-net-development-services'
+                    },
+                    {
+                        subLink: 'Java',
+                        route: 'skills/java-development-services'
+                    }
+                ]
+            },
+            {
+                subtitle: 'The best of JS',
+                route: 'skills/best-of-js',
+                subLinks: [
+                    {
+                        subLink: 'React',
+                        route: 'skills/react-development-services'
+                    },
+                    {
+                        subLink: 'Angular',
+                        route: 'skills/angular-development-services'
+                    },
+                    {
+                        subLink: 'Node',
+                        route: 'skills/node-development-services'
+                    },
+                    {
+                        subLink: 'Vue',
+                        route: 'skills/vue-development-services'
+                    }
+                ]
+            }
+        ],
+        isArrow: true
+    },
+    {
+        title: 'Solutions',
+        route: 'solutions',
+        links: [
+            {
+                subtitle: 'Industries',
+                route: 'solutions/industries',
+                subLinks: [
+                    {
+                        subLink: 'FinTech',
+                        route: 'solutions/fintech'
+                    },
+                    {
+                        subLink: 'InsurTech',
+                        route: 'solutions/insurance'
+                    },
+                    {
+                        subLink: 'HealthTech',
+                        route: 'solutions/healthtech'
+                    },
+                    {
+                        subLink: 'EdTech',
+                        route: 'solutions/elearning'
+                    },
+                    {
+                        subLink: 'Ecommerce',
+                        route: 'solutions/ecommerce'
+                    },
+                    {
+                        subLink: 'Real Estate',
+                        route: 'solutions/real-estate'
+                    },
+                    {
+                        subLink: 'Marketing and AdTech',
+                        route: 'solutions/adtech'
+                    }
+                ]
+            },
+            {
+                subtitle: 'Emerging technologies',
+                route: 'solutions/emerging-technologies',
+                subLinks: [
+                    {
+                        subLink: 'Big data',
+                        route: 'solutions/big-data'
+                    },
+                    {
+                        subLink: 'Artificial intelligence',
+                        route: 'solutions/ai'
+                    },
+                    {
+                        subLink: 'AR/VR',
+                        route: 'solutions/ar-vr'
+                    },
+                    {
+                        subLink: 'Cybersecurity',
+                        route: 'solutions/cybersecurity'
+                    },
+                    {
+                        subLink: 'Internet of things',
+                        route: 'solutions/iot'
+                    },
+                    {
+                        subLink: 'Business intelligence',
+                        route: 'solutions/business-intelligence'
+                    },
+                    {
+                        subLink: 'Blockchain',
+                        route: 'solutions/blockchain'
+                    },
+                    {
+                        subLink: 'Serverless Applications',
+                        route: 'solutions/serverless-solutions'
+                    }
+                ]
+            }
+        ],
+        isArrow: true
+    },
+    {
+        title: 'Case studies',
+        route: 'our-works',
+        links: [
+            {
+                subtitle: 'Go to portfolio',
+                route: 'our-works'
+            },
+            {
+                subtitle: 'Technology',
+                subLinks: [
+                    {
+                        subLink: 'Angular',
+                        route: '/our-works/?tags=angular'
+                    },
+                    {
+                        subLink: 'React',
+                        route: '/our-works/?tags=react'
+                    },
+                    {
+                        subLink: 'Salesforce',
+                        route: '/our-works/?tags=salesforce'
+                    },
+                    {
+                        subLink: 'Node',
+                        route: '/our-works/?tags=node'
+                    },
+                    {
+                        subLink: 'Mobile',
+                        route: '/our-works/?tags=mobile'
+                    },
+                    {
+                        subLink: 'PHP',
+                        route: '/our-works/?tags=php'
+                    },
+                    {
+                        subLink: 'Ruby on Rails',
+                        route: '/our-works/?tags=ror'
+                    },
+                    {
+                        subLink: 'React Native',
+                        route: '/our-works/?tags=react-native'
+                    },
+                    {
+                        subLink: 'Java',
+                        route: '/our-works/?tags=java'
+                    },
+                    {
+                        subLink: 'Python',
+                        route: '/our-works/?tags=python'
+                    },
+                    {
+                        subLink: 'QA and testing',
+                        route: '/our-works/?tags=qa'
+                    }
+                ]
+            }
+        ],
+        isArrow: true
+    },
+    {
+        title: 'How we work',
+        route: 'company/team-as-a-service',
+        isArrow: false
+    },
+    {
+        title: 'Portfolio',
+        route: 'our-works',
+        isArrow: false
+    },
+    {
+        title: 'Blog',
+        route: 'blog',
+        isArrow: false
+    },
+    {
+        title: 'About Us',
+        route: 'company',
+        isArrow: false
+    }
+];
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { Hamburger, Logo, Divider, defaultTheme } from 'itechartsitewrapper';
+import { Hamburger, Logo, Divider, defaultTheme, LinkAlignedWithIcon, LinkWrapper, LinkArrow, MenuSubLinks, MenuLinkList } from 'itechartsitewrapper';
+import arrow from '../assets/images/arrow.svg';
+import MENU_LINKS from '../mocks/MENU_LINKS';
 
 export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isLinkActive: false };
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    }
+
+    handleMouseEnter() {
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                isLinkActive : !prevState.isLinkActive
+            }
+        })
+    }
+
   render () {
     return (
       <ThemeProvider theme={defaultTheme}>
           <Hamburger color='red' />
           <Logo />
           <Divider />
+          <div style={{background: 'black', height: '30vh'}}>
+              <LinkAlignedWithIcon
+                  text="Link with icon"
+                  icon={arrow.src}
+                  size={40}
+                  fontSize={50}
+                  fontWeight={800}
+                  colorOnHover="red"
+                  color="pink"
+                  position="down"
+                  isLinkActive={this.state.isLinkActive}
+                  onMouseEnter={this.handleMouseEnter}
+                  onClick={() => console.log('test onclick linkAlignedWithIcon')}
+              />
+          </div>
+          <LinkWrapper
+              text="link wrapper"
+              fontSize={100}
+              color="green"
+              colorOnHover="yellow"
+              textTransform="uppercase"
+              onClick={() => console.log('test on click')}
+              onMouseEnter={() => console.log('test on mouse enter')}
+          />
+          <div style={{background: 'black', height: '100vh'}}>
+              <MenuSubLinks
+                  subtitle="jakis subtitle"
+                  route="someRoute"
+                  color="yellow"
+                  colorOnHover="green"
+                  fontSize={35}
+                  fontWeight={400}
+              />
+              <MenuLinkList
+                  mocks={MENU_LINKS}
+              />
+          </div>
       </ThemeProvider>
     );
   }
