e02b31565ef3b889286af6b022fa2f567a0e14a6
Magdalena Oziębło <magdalena.ozieblo@itechart-group.com>
2021-08-13 16:05:21 +0200

    updated version

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
-import { Hamburger, Logo, Divider, defaultTheme, LinkAlignedWithIcon, LinkWrapper, LinkArrow, MenuSubLinks, MenuLinkList } from 'itechartsitewrapper';
-import arrow from '../assets/images/arrow.svg';
-import MENU_LINKS from '../mocks/MENU_LINKS';
+import { defaultTheme } from 'itechartsitewrapper';
 
 export default class extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { isLinkActive: false };
-        this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    }
-
   render () {
     return (
       <ThemeProvider theme={defaultTheme}>
-        <CssBaseline /> 
+        <CssBaseline />
       </ThemeProvider>
     );
   }
