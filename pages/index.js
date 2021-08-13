31f6110b3aab36f7f5f5a32c5ccc15afcc11ac3c
Wojciech Mosienski <wojciech.mosienski@itechart-group.com>
2021-08-13 10:31:31 +0200

    add theme provider

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
-import { Hamburger } from 'itechartsitewrapper'
+import React, { Component } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Hamburger, Logo, Divider, defaultTheme } from 'itechartsitewrapper';
 
 export default class extends Component {
   render () {
     return (
-        <>
+      <ThemeProvider theme={defaultTheme}>
           <Hamburger color='red' />
-        </>
+          <Logo />
+          <Divider />
+      </ThemeProvider>
     );
   }
 }
