8001d4326b2172a45a4d7684a1178c8f9a86f710
Wojciech Mosienski <wojciech.mosienski@itechart-group.com>
2021-08-17 12:55:34 +0200

    wip

--- components/SiteHeader.jsx
+++ components/SiteHeader.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { SiteHeader, Hamburger, Logo } from 'itechartsitewrapper';
 import WithScrolled from 'itechartsitewrapper/dist/core/helpers/WithScrolled/';
+import LinkAlignedWithIcon from 'itechartsitewrapper/dist/core/LinkAlignedWithIcon';
+import dot from '../assets/images/dot.svg';
 
 const SiteHeaderExample = ({ scrolled, ...siteHeaderProps }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +13,22 @@ const SiteHeaderExample = ({ scrolled, ...siteHeaderProps }) => {
           shouldBeVisible={scrolled}
           isOpen={isOpen}
           leftNodes={[<Logo key="logo" />]}
-          rightNodes={[<Hamburger key="hamburger" color="red" onClick={toggleOpen} />]}
+          rightNodes={[
+            <LinkAlignedWithIcon 
+              text='How we work'
+              position='down'
+              size={10}
+              isLinkActive={true}
+              icon={dot.src}
+              onMouseEnter={() => {}}
+              fontSize={24}
+              fontWeight={400}
+              textTransform='inherit'
+              color='#868686'
+              colorOnHover='#FFFFFF'
+            />,
+            <Hamburger key="hamburger" color="red" onClick={toggleOpen} />
+          ]}
           {...siteHeaderProps}
       />
   );
